2f763f2363ad371d802eb05fef55e3067c29b196
felipezardo <zetazarudo@proton.me>
2026-03-07 09:57:11 -0300

    W01 Arrays aSSIGNMENt

---
+++ week01/code/Arrays.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+
+public static class Arrays
+{
+    /// <summary>
+    /// This function will produce an array of size 'length' starting with 'number' followed by multiples of 'number'.  For 
+    /// example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.  Assume that length is a positive
+    /// integer greater than 0.
+    /// </summary>
+    /// <returns>array of doubles that are the multiples of the supplied number</returns>
+    public static double[] MultiplesOf(double number, int length)
+    {
+        // TODO Problem 1 Start
+        // Plan:
+        // 1. Create array of doubles, size is 'length'.
+        // 2. Make a loop for the length.
+        // 3. Calculate: number * (i + 1).
+        // 4. Save result in the array.
+        // 5. Return the array.
+
+        double[] multiples = new double[length];
+        
+        for (int i = 0; i < length; i++)
+        {
+            multiples[i] = number * (i + 1);
+        }
+
+        return multiples;
+    }
+
+    /// <summary>
+    /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
+    /// List<int>{1, 2, 3, 4, 5, 6, 7, 8, 9} and an amount is 3 then the list after the function runs should be 
+    /// List<int>{7, 8, 9, 1, 2, 3, 4, 5, 6}.  The value of amount will be in the range of 1 to data.Count, inclusive.
+    ///
+    /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
+    /// </summary>
+    public static void RotateListRight(List<int> data, int amount)
+    {
+        // TODO Problem 2 Start
+        // Plan:
+        // 1. Find index to cut. data.Count minus amount.
+        // 2. Copy the last numbers to a temporary list (GetRange).
+        // 3. Delete these numbers from the end of original list (RemoveRange).
+        // 4. Put the copied numbers at the beginning, index 0 (InsertRange).
+
+        int startIndex = data.Count - amount;
+        
+        List<int> tailElements = data.GetRange(startIndex, amount);
+        
+        data.RemoveRange(startIndex, amount);
+        
+        data.InsertRange(0, tailElements);
+    }
+}
